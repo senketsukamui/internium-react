10e3880195286a1f2aecf33e5d1d2e6d842d6572
Gleb Potapov <gleb.potapov.2001@gmail.com>
2022-03-28 19:52:28 +0400

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       pages: path.resolve(__dirname, "./src/pages"),
       assets: path.resolve(__dirname, "./src/assets"),
       styles: path.resolve(__dirname, "./src/styles"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
     },
   },
 });
