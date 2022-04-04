bc6b69d23ff9db75df53942b167d90242d20c274
senketsukamui <glebpotapov2001@gmail.com>
2022-04-04 23:14:50 +0300

    Update configs

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       styles: path.resolve(__dirname, "./src/styles"),
       hooks: path.resolve(__dirname, "./src/hooks"),
       api: path.resolve(__dirname, "./src/api"),
+      utils: path.resolve(__dirname, "./src/utils"),
     },
   },
 });
