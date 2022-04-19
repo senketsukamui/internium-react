20d907c93d642e6d74ff8e227987ae823e56f152
senketsukamui <glebpotapov2001@gmail.com>
2022-04-19 21:53:23 +0300

    Implement notistack hook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,20 +6,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CompanyRegister from "pages/Auth/CompanyAuth/Register";
 import CompanyLogin from "pages/Auth/CompanyAuth/Login";
 import StudentAuth from "pages/Auth/StudentAuth/Login";
+import { SnackbarProvider } from "notistack";
 
 function App() {
   return (
     <div className={s.app}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path="auth" element={<Auth />} />
-          // TODO: Refactor this
-          <Route path="auth/company/register" element={<CompanyRegister />} />
-          <Route path="auth/company/login" element={<CompanyLogin />} />
-          <Route path="auth/student" element={<StudentAuth />} />
-        </Routes>
-      </BrowserRouter>
+      <SnackbarProvider maxSnack={4}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Home />} />
+            <Route path="auth" element={<Auth />} />
+            // TODO: Refactor this
+            <Route path="auth/company/register" element={<CompanyRegister />} />
+            <Route path="auth/company/login" element={<CompanyLogin />} />
+            <Route path="auth/student" element={<StudentAuth />} />
+          </Routes>
+        </BrowserRouter>
+      </SnackbarProvider>
     </div>
   );
 }
