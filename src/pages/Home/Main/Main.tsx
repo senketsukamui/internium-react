07df8bb1d3dc369cd85a39c18229060eacfb19ef
senketsukamui <glebpotapov2001@gmail.com>
2022-05-18 20:41:13 +0400

    Update main page

--- src/pages/Home/Main/Main.tsx
+++ src/pages/Home/Main/Main.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import { BorderedInput } from "components/BorderedInput/BorderedInput";
+import { useStores } from "hooks/useStores";
+import { observer } from "mobx-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import bgMain from "../../../assets/images/bg-main.jpeg";
@@ -7,6 +9,7 @@ import { ButtonModes } from "./constants";
 
 const Main = () => {
   const navigate = useNavigate();
+  const { authStore } = useStores();
   const [alignment, setAlignment] = React.useState<ButtonModes>(
     ButtonModes.Students
   );
@@ -76,74 +79,85 @@ const Main = () => {
                     </Typography>
                   </Grid>
 
-                  <Grid
-                    item
-                    container
-                    sx={(theme) => ({ marginTop: theme.spacing(5) })}
-                    wrap="nowrap"
-                  >
-                    <Grid container item>
-                      <BorderedInput
-                        placeholder="Позиция, навыки или компания"
-                        sx={{ width: "100%" }}
-                      />
+                  <form onSubmit={handleSearchSubmit}>
+                    <Grid
+                      item
+                      container
+                      sx={(theme) => ({ marginTop: theme.spacing(5) })}
+                      wrap="nowrap"
+                    >
+                      <Grid container item>
+                        <BorderedInput
+                          placeholder="Позиция, навыки или компания"
+                          sx={{ width: "100%" }}
+                          value={search}
+                          onChange={(e) => setSearch(e.target.value)}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          color="success"
+                          variant="contained"
+                          type="submit"
+                          sx={{
+                            height: "100%",
+                            borderRadius: 0,
+                            borderTopRightRadius: 3,
+                            borderBottomRightRadius: 3,
+                            marginLeft: "-2px",
+                            zIndex: 99,
+                            whiteSpace: "nowrap",
+                            backgroundColor: "#008563",
+                          }}
+                        >
+                          Перейти к поиску
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Grid>
+              </Grid>
+
+              {!authStore.getUserObject && (
+                <Grid
+                  item
+                  sm={4}
+                  sx={{
+                    background: "#E1F5FE",
+                    padding: "64px 32px 32px 32px",
+                  }}
+                >
+                  <Grid container direction="column">
+                    <Grid item>
+                      <Typography variant="body1" fontWeight="600">
+                        Вы работодатель?
+                      </Typography>
                     </Grid>
                     <Grid item>
-                      <Button
-                        color="success"
-                        variant="contained"
-                        sx={{
-                          height: "100%",
-                          borderRadius: 0,
-                          borderTopRightRadius: 3,
-                          borderBottomRightRadius: 3,
-                          marginLeft: "-2px",
-                          zIndex: 99,
-                          whiteSpace: "nowrap",
-                          backgroundColor: "#008563",
-                        }}
+                      <Typography
+                        variant="h4"
+                        fontWeight="700"
+                        sx={{ fontSize: "1.6rem" }}
                       >
-                        Перейти к поиску
+                        Опубликуйте вакансии
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      sx={(theme) => ({ marginTop: theme.spacing(5) })}
+                    >
+                      <Button
+                        color="primary"
+                        variant="contained"
+                        href="/auth/company/register"
+                        sx={{ height: 50, marginTop: "2px" }}
+                      >
+                        Создать Вакансию
                       </Button>
                     </Grid>
                   </Grid>
                 </Grid>
-              </Grid>
-
-              <Grid
-                item
-                sm={4}
-                sx={{
-                  background: "#E1F5FE",
-                  padding: "64px 32px 32px 32px",
-                }}
-              >
-                <Grid container direction="column">
-                  <Grid item>
-                    <Typography variant="body1" fontWeight="600">
-                      Вы работодатель?
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <Typography
-                      variant="h4"
-                      fontWeight="700"
-                      sx={{ fontSize: "1.6rem" }}
-                    >
-                      Опубликуйте вакансии
-                    </Typography>
-                  </Grid>
-                  <Grid item sx={(theme) => ({ marginTop: theme.spacing(5) })}>
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      sx={{ height: 50, marginTop: "2px" }}
-                    >
-                      Создать Вакансию
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Grid>
+              )}
             </Grid>
           </Paper>
         </Box>
@@ -152,4 +166,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default observer(Main);
